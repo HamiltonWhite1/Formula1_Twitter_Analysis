0cc90b907f4a91eb0a207da650edc0e3733d21d4
HamiltonWhite1 <92882456+HamiltonWhite1@users.noreply.github.com>
2022-03-31 12:46:02 -0400

    Refactored Twitter scrape to get cleaner more useable data, and cleaned up the cleaning Jupyter file

---
+++ twitter_call.py
@@ -0,0 +1,37 @@
+from apify_client import ApifyClient
+
+# Initialize the ApifyClient with your API token
+client = ApifyClient("apify_api_1iY9IFnf6GSNO0SRqXZghRKxk700ET2va3YS")
+
+# Prepare the actor input
+run_input = {
+    "searchTerms": [
+    "formula 1",
+    "BahrainGP",
+    "F1",
+    "f1",
+    "FORMULA1"
+  ],
+    "searchMode": "",
+    "mode": "replies",
+    "tweetsDesired": 1000,
+    "proxyConfig": { "useApifyProxy": True },
+    "extendOutputFunction": """async ({ data, item, page, request, customData, Apify }) => {
+  return item;
+}""",
+    "extendScraperFunction": """async ({ page, request, addSearch, addProfile, _, addThread, addEvent, customData, Apify, signal, label }) => {
+ 
+}""",
+    "customData": {},
+    "handlePageTimeoutSecs": 5000,
+    "maxRequestRetries": 3,
+    "maxIdleTimeoutSecs": 30,
+    "initialCookies": [],
+}
+
+# Run the actor and wait for it to finish
+run = client.actor("vdrmota/twitter-scraper").call(run_input=run_input)
+
+# Fetch and print actor results from the run's dataset (if there are any)
+for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+    print(item)
